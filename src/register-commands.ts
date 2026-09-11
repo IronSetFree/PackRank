4b33f524d4d1469b3c6e6e571183b32d6ffd4410
IronSetFree <nathanaholland95@gmail.com>
2026-09-10 22:27:16 -0400

    refactor: reuse Discord command registration helper

--- src/register-commands.ts
+++ src/register-commands.ts
@@ -1,20 +1,3 @@
-import { REST, Routes } from "discord.js";
-import { commandDefinitions } from "./commands/definitions.js";
-import { config } from "./config.js";
+import { registerCommands } from "./commands/register.js";
 
-const rest = new REST({ version: "10" }).setToken(config.DISCORD_TOKEN);
-const body = commandDefinitions.map(command => command.toJSON());
-
-await rest.put(
-  Routes.applicationCommands(config.DISCORD_CLIENT_ID),
-  { body }
-);
-console.log(`Deployed ${body.length} global commands.`);
-
-if (config.DISCORD_GUILD_ID) {
-  await rest.put(
-    Routes.applicationGuildCommands(config.DISCORD_CLIENT_ID, config.DISCORD_GUILD_ID),
-    { body }
-  );
-  console.log(`Deployed ${body.length} guild commands to ${config.DISCORD_GUILD_ID}.`);
-}
+await registerCommands();
