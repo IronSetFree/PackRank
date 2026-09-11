d69bd5514cf8f56ce3f2e051502a83707f540810
IronSetFree <nathanaholland95@gmail.com>
2026-09-10 22:18:22 -0400

    fix: register global commands and test guild commands

--- src/register-commands.ts
+++ src/register-commands.ts
@@ -5,13 +5,16 @@ import { config } from "./config.js";
 const rest = new REST({ version: "10" }).setToken(config.DISCORD_TOKEN);
 const body = commandDefinitions.map(command => command.toJSON());
 
+await rest.put(
+  Routes.applicationCommands(config.DISCORD_CLIENT_ID),
+  { body }
+);
+console.log(`Deployed ${body.length} global commands.`);
+
 if (config.DISCORD_GUILD_ID) {
   await rest.put(
     Routes.applicationGuildCommands(config.DISCORD_CLIENT_ID, config.DISCORD_GUILD_ID),
     { body }
   );
   console.log(`Deployed ${body.length} guild commands to ${config.DISCORD_GUILD_ID}.`);
-} else {
-  await rest.put(Routes.applicationCommands(config.DISCORD_CLIENT_ID), { body });
-  console.log(`Deployed ${body.length} global commands.`);
 }
